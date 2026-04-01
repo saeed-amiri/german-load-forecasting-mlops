9752e0f0feb119a49c269e27a90705734055dc8e
saeed amiri <sdramiri952@gmail.com>
2026-04-01 14:12:04 +0200

    before upgrading

--- services/auth/app/main.py
+++ services/auth/app/main.py
@@ -6,9 +6,11 @@ from fastapi import FastAPI
 from ..routes.loging import router as login_router
 from ..routes.protected import router as protected_router
 from ..routes.register import router as register_router
+from ..routes.test_rbac import router as rbac_router
 
 app = FastAPI()
 
 app.include_router(login_router, prefix="/auth")
 app.include_router(register_router, prefix="/auth")
 app.include_router(protected_router, prefix="/auth")
+app.include_router(rbac_router, prefix="/auth")
