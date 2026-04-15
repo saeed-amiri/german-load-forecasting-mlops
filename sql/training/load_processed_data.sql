c2e9402159e77b359c0a38567a58a3e7b48dd045
saeed amiri <sdramiri952@gmail.com>
2026-04-15 19:39:48 +0200

    get only the required columns

--- sql/training/load_processed_data.sql
+++ sql/training/load_processed_data.sql
@@ -1,6 +1,10 @@
 -- sql/training/read_processed.sql
 
-SELECT * FROM read_parquet('{{ path }}')
+SELECT 
+    {% for col in columns %}
+        {{ col }} {% if not loop.last %},{% endif %}
+    {% endfor %}
+FROM read_parquet('{{ path }}')
 WHERE
     {% for col in columns %}
         {{ col }} IS NOT NULL {% if not loop.last %}AND{% endif %}
